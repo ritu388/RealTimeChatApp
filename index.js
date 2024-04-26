34ca43527a3d5091153b8d0134c2e77751b19715
ritu388 <ritup665@gmail.com>
2024-04-26 14:05:42 +0530

    database is connected

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const DBURL  = require('./DBcofig');
+
+mongoose.connect(DBURL.DB_URI,{ useNewUrlParser: true , useUnifiedTopology: true });
+
+mongoose.connection.on('open', function(){
+    console.log('data base is connected');
+});
+
+mongoose.connection.on('error', function(){
+    console.error('data base is not connected');
+});
+
 
 
 app.listen(5000, function(){
     console.log('app is listening on port 5000');
-})
+});
